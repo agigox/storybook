acbd6c8fa2a3dc6db1df23a95b3eec1e7b2b0bd6
Amine TABOU <amin.tabou@gmail.com>
2022-10-01 09:38:12 +0200

    Storybook learning

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Button from "./components/Button";
+import Stack from "./components/Stack";
 
 function App() {
+  const handleClick = () => "Hello";
+  const style = {
+    width: "50px",
+    height: "50px",
+    backgroundColor: "red",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>COMPONENT 1</h1>
+      <Button
+        label="Press Me"
+        backgroundColor="red"
+        size="md"
+        handleClick={handleClick}
+      ></Button>
+      <h1>COMPONENT 2</h1>
+      <Stack>
+        <div style={style}>1</div>
+        <div style={style}>2</div>
+        <div style={style}>3</div>
+        <div style={style}>4</div>
+        <div style={style}>5</div>
+        <div style={style}>6</div>
+        <div style={style}>7</div>
+        <div style={style}>8</div>
+      </Stack>
+    </>
   );
 }
 
